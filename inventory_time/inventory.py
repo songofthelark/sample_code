07cfaae04305983133be8a83a7fa2bf8e36e2cc1
Erin <108779879+songofthelark@users.noreply.github.com>
2026-04-17 21:29:18 -0400

    add putback function to put back stickered item

--- inventory_time/inventory.py
+++ inventory_time/inventory.py
@@ -7,7 +7,6 @@ import argparse
 
 
 def eastern_time(str_date):
-
     dateformat = "%Y-%m-%d %H:%M:%S"
     naive_dt = datetime.strptime(str_date, dateformat)
     utc_dt = naive_dt.replace(tzinfo=ZoneInfo("UTC"))
@@ -15,11 +14,12 @@ def eastern_time(str_date):
 
     return eastern_dt.strftime(dateformat)
 
+
 def error(msg):
     print("FAILED", msg)
 
-def item_exists(con: Connection, item_id) -> bool:
 
+def item_exists(con: Connection, item_id) -> bool:
     res = con.cursor().execute(f"select count(*) from items where item_id = {item_id}")
     count = res.fetchone()[0]
 
@@ -31,13 +31,12 @@ def item_exists(con: Connection, item_id) -> bool:
 
 def item_needs_sticker(con, item_id):
     res = con.cursor().execute(f"select needs_sticker from items where item_id = {item_id}")
-    needs_sticker  = res.fetchone()[0]
+    needs_sticker = res.fetchone()[0]
 
     return needs_sticker
 
 
 def clear_all_tables(con):
-
     confirm = input("Type YES all caps if you mean it: ")
     if confirm.strip() != "YES":
         print("nevermind then")
@@ -52,14 +51,13 @@ def clear_all_tables(con):
 
 
 def list_items(con: Connection, cmd):
-
     # can specify a search term to list
     ar = cmd.split(" ")
 
     sql = "select * from items"
 
-    #list hidden means
-    if len(ar) > 1 :
+    # list hidden means
+    if len(ar) > 1:
         if ar[1] == "hidden":
             sql += " where hide=1 "
         else:
@@ -75,8 +73,7 @@ def list_items(con: Connection, cmd):
         print(f"{row[0]}: {row[1]}")
 
 
-def recalculate_inventory(con:Connection, table_type, item_id:int, quantity_changed:int, sticker:int =0):
-
+def recalculate_inventory(con: Connection, table_type, item_id: int, quantity_changed: int, sticker: int = 0):
     # clunky sql placeholders for the extra parameter we need for box
     sph1 = sph2 = sph3 = ""
     if table_type == "box":
@@ -100,27 +97,27 @@ def recalculate_inventory(con:Connection, table_type, item_id:int, quantity_chan
     else:
 
         cur.execute((f"update {table_type}_inventory set quantity=quantity+{quantity_changed} "
-                    f"where item_id = {item_id} {sph3}"))
+                     f"where item_id = {item_id} {sph3}"))
 
     con.commit()
     return True
 
 
 def show_max_sticker(con):
-
     max_sticker = 0
     tables = ['box_inventory', 'expiration_dates']
     for table in tables:
-        s =  f"select max(sticker) from {table}"
+        s = f"select max(sticker) from {table}"
         res = con.cursor().execute(s)
         row = res.fetchone()
         if row and row[0] and int(row[0]) > max_sticker:
             max_sticker = int(row[0])
 
     if max_sticker:
-        print(f"Last sticker was {max_sticker}" )
+        print(f"Last sticker was {max_sticker}")
     else:
         print("No stickers")
+
 
 def get_item_name_quantity(con, table_type, item_id):
     s = (f"select i.item_id, item_name, sum(quantity) from {table_type}_inventory x "
@@ -130,14 +127,14 @@ def get_item_name_quantity(con, table_type, item_id):
     row = res.fetchone()
     if row:
         return row[1], row[2]
-    return  "(none)", 0
-
-def add_exp_date(con, cmd):
-    pass
+    return "(none)", 0
 
 
-def warehouse_transaction(con:Connection, cmd):
+# def add_exp_date(con, cmd):
+#    pass
 
+
+def warehouse_transaction(con: Connection, cmd):
     item_id, quantity_changed, sticker = check_cmd_line(cmd)
     if item_id <= 0 or quantity_changed == 0:
         error("Bad command, should be wadd i# q#")
@@ -155,10 +152,10 @@ def warehouse_transaction(con:Connection, cmd):
 
     if box_quantity > warehouse_quantity + quantity_changed:
         error(f"You would have more {name} in the box than the warehouse if you did this")
-        #print(f"Box has {box_quantity}, warehouse has {warehouse_quantity}")
+        # print(f"Box has {box_quantity}, warehouse has {warehouse_quantity}")
         return
 
-    recalculate_inventory(con, "warehouse",  item_id, quantity_changed)
+    recalculate_inventory(con, "warehouse", item_id, quantity_changed)
 
     sql = (f"insert into warehouse_transactions (item_id, quantity_changed) "
            f"values ({item_id}, {quantity_changed})")
@@ -169,8 +166,8 @@ def warehouse_transaction(con:Connection, cmd):
     item_name, quantity = get_item_name_quantity(con, "warehouse", item_id)
     print(f"Warehouse now has {quantity} {item_name} id {item_id}")
 
-def check_cmd_line(cmd):
 
+def check_cmd_line(cmd):
     item_id = -1
     quantity = -1
     sticker = 0
@@ -195,7 +192,7 @@ def check_cmd_line(cmd):
     if len(matches) == 1:
         sticker = int(matches[0][1:])
         if sticker <= 0:
-            error(f"Invalid stucker number {sticker}")
+            error(f"Invalid sticker number {sticker}")
             return item_id, quantity, sticker
 
         if cmd.startswith("badd"):
@@ -208,16 +205,13 @@ def check_cmd_line(cmd):
         if len(matches) == 1:
             quantity = int(matches[0][1:])
 
-    if quantity ==  0:
+    if quantity == 0:
         error("Zero quantity?")
-
-
 
     return item_id, quantity, sticker
 
 
 def box_transaction(con, cmd):
-
     item_id, quantity_changed, sticker = check_cmd_line(cmd)
     if item_id <= 0 or quantity_changed == 0:
         error("Bad command, should be badd i# q# [s#]")
@@ -240,7 +234,6 @@ def box_transaction(con, cmd):
     if warehouse_quantity < (quantity_changed + box_quantity):
         error(f"Sorry you only have {warehouse_quantity} of item {item_id} in the warehouse.")
         return
-
 
     if quantity_changed < 0 and abs(quantity_changed) > box_quantity:
         error(f"Sorry you only have {box_quantity} of item {item_id} in the box.")
@@ -282,12 +275,13 @@ def box_transaction(con, cmd):
             print(f"There is or was already an item in the box with sticker {sticker}: {item_name} (id {item_id})")
             return
 
-    success = recalculate_inventory(con, table_type="box",  item_id=item_id, quantity_changed=quantity_changed, sticker=sticker)
+    success = recalculate_inventory(con, table_type="box", item_id=item_id, quantity_changed=quantity_changed,
+                                    sticker=sticker)
     if not success:
         return
 
     if quantity_changed < 0:
-        recalculate_inventory(con, table_type="warehouse",  item_id=item_id, quantity_changed=quantity_changed)
+        recalculate_inventory(con, table_type="warehouse", item_id=item_id, quantity_changed=quantity_changed)
 
     sql = (f"insert into box_transactions (item_id, quantity_changed, sticker) "
            f"values ({item_id}, {quantity_changed}, {sticker})")
@@ -300,8 +294,7 @@ def box_transaction(con, cmd):
         print(f"{table_type.title()} now has {quantity} {item_name} id {item_id}")
 
 
-def show_box_restock(con:Connection):
-
+def show_box_restock(con: Connection):
     print("---------------------------------")
     print("BOX OUT OF STOCK")
     print("---------------------------------")
@@ -318,7 +311,7 @@ def show_box_restock(con:Connection):
         warehouse[item_id] = int(quantity)
 
     sql = (f"select i.item_id, item_name, sum(quantity) as qsum from box_inventory w "
-            f"join items i  on w.item_id = i.item_id where hide=0 group by 1,2 having qsum = 0")
+           f"join items i  on w.item_id = i.item_id where hide=0 group by 1,2 having qsum = 0")
 
     res = con.cursor().execute(sql)
     rows = res.fetchall()
@@ -332,6 +325,7 @@ def show_box_restock(con:Connection):
             line += f"\twarehouse has: {warehouse[item_id]}"
 
         print(line)
+
 
 def show_warehouse_restock(con: Connection):
     print("---------------------------------")
@@ -354,10 +348,9 @@ def show_warehouse_restock(con: Connection):
 
 
 def show_inventory(con: Connection, table_type, cmd=""):
-
-    list_by_stickers=False
+    list_by_stickers = False
     ar = cmd.split(" ")
-    if table_type== "box" and  len(ar) == 2 and ar[1].startswith("sticker"):
+    if table_type == "box" and len(ar) == 2 and ar[1].startswith("sticker"):
         list_by_stickers = True
 
     item_to_search_for = ""
@@ -370,16 +363,15 @@ def show_inventory(con: Connection, table_type, cmd=""):
            f"{table_type.upper()} INVENTORY",
            "---------------------------------"]
 
-    if table_type == "box" and  list_by_stickers:
+    if table_type == "box" and list_by_stickers:
         sql = f"select i.item_id, item_name, sticker, sum(quantity) from {table_type}_inventory w "
 
     else:
         sql = f"select i.item_id, item_name, 0 as sticker, sum(quantity) from {table_type}_inventory w "
 
-
-    sql +=  f"join items i on w.item_id = i.item_id where quantity > 0 "
+    sql += f"join items i on w.item_id = i.item_id where quantity > 0 "
     if item_to_search_for:
-        sql +=  f" and i.item_id = {item_to_search_for} "
+        sql += f" and i.item_id = {item_to_search_for} "
 
     sql += " group by 1,2,3"
 
@@ -392,8 +384,8 @@ def show_inventory(con: Connection, table_type, cmd=""):
         else:
             out.append(f"No items in {table_type}_inventory")
 
-    for row in rows :
-        item_id, name, sticker, quantity  = row
+    for row in rows:
+        item_id, name, sticker, quantity = row
         line = f"id {item_id} {name}, units: {quantity}"
         if sticker:
             line += f", sticker: {sticker}"
@@ -405,10 +397,8 @@ def show_inventory(con: Connection, table_type, cmd=""):
     return "\n".join(out)
 
 
-
 def show_transactions(con: Connection, table_type, cmd):
-
-    limit=1000
+    limit = 1000
     ar = cmd.split(" ")
     if len(ar) == 2:
         limit = ar[1]
@@ -416,7 +406,8 @@ def show_transactions(con: Connection, table_type, cmd):
     if table_type == "box":
         sql = f"select i.item_id, item_name, quantity_changed, sticker, last_updated from {table_type}_transactions w "
     else:
-        sql =f"select i.item_id, item_name, quantity_changed, 0 as sticker, last_updated from {table_type}_transactions w "
+        sql = (f"select i.item_id, item_name, quantity_changed, 0 as sticker, last_updated from "
+               f"{table_type}_transactions w ")
 
     sql += f"join items i  on w.item_id = i.item_id order by last_updated desc limit {limit}"
 
@@ -430,10 +421,10 @@ def show_transactions(con: Connection, table_type, cmd):
             sticker_str = f"sticker: {sticker}"
         print(f"id {item_id} {name}: {quantity} {sticker_str} {eastern_time(last_updated)}")
 
-    print(n+1, "rows")
+    print(n + 1, "rows")
 
-def execute_sql(con,cmd):
 
+def execute_sql(con, cmd):
     sql = cmd[4:]
     res = con.cursor().execute(sql)
 
@@ -445,12 +436,11 @@ def execute_sql(con,cmd):
 
 
 def totals(con, summarized=True, diapers_only=False):
-
     sql = ("select i.item_id, item_name, sum(quantity_changed) qsum "
            "from box_transactions b join items i on b.item_id = i.item_id"
-           " where quantity_changed < 0 " )
+           " where quantity_changed < 0 ")
     if diapers_only:
-            sql += " and item_name like 'diaper%' "
+        sql += " and item_name like 'diaper%' "
 
     sql += "group by 1,2 having qsum < 0 order by qsum desc"
 
@@ -466,14 +456,12 @@ def totals(con, summarized=True, diapers_only=False):
              "toothpaste": {14, 31},
              "toothbrushes": {17, 18},
              "moisturizer": {35, 21},
-             "soap": {20,32},
-             "pads": {1,37},
-             "baby wash/lotion": {25,36,24},
-             "shampoo": {34,28},
-             "conditioner": {30,33},
-             "hairbrushes": {12,13}}
-
-
+             "soap": {20, 32},
+             "pads": {1, 37},
+             "baby wash/lotion": {25, 36, 24},
+             "shampoo": {34, 28},
+             "conditioner": {30, 33},
+             "hairbrushes": {12, 13}}
 
     categories = {}
     for item in items:
@@ -498,9 +486,7 @@ def totals(con, summarized=True, diapers_only=False):
             print(f"{abs(quantity)} {item_name}")
 
 
-
 def dump_to_text(con):
-
     date_str = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
     with open(f"dump_{date_str}.txt", 'w') as f:
 
@@ -523,8 +509,6 @@ def dump_to_text(con):
 
 
 def restock_by_sticker(con):
-
-
     sql = (f"select i.item_id, item_name, sticker from box_inventory b join "
            f" items i on b.item_id=i.item_id where sticker <> 0 "
            f"and quantity > 0 order by sticker, item_name")
@@ -550,15 +534,12 @@ def restock_by_sticker(con):
             print(f"Sticker {stk} isn't there")
             continue
 
-
-
         stickers.add(int(stk))
 
     if not stickers:
         return
 
-
-    found_stickers =  []
+    found_stickers = []
     removed_stickers = []
 
     for row in all_box_items:
@@ -568,7 +549,6 @@ def restock_by_sticker(con):
             found_stickers.append((item_id, item_name, box_sticker))
         else:
             removed_stickers.append((item_id, item_name, box_sticker))
-
 
     print("So these items are in the box: ")
     for item_id, name, sticker in found_stickers:
@@ -608,7 +588,6 @@ def hide(con, cmd):
 
 
 def restock_no_sticker(con):
-
     res = con.cursor().execute("select i.item_id, item_name, quantity from items i"
                                " join box_inventory b on b.item_id = i.item_id"
                                " where needs_sticker=0 and quantity>0")
@@ -636,7 +615,7 @@ def restock_no_sticker(con):
         for item_id, name, quantity, new_quantity in items_changed_quantity:
             items_removed = quantity - int(new_quantity)
             print(f"{items_removed} units {name}, now have {new_quantity}")
-            removed.append( (item_id, items_removed))
+            removed.append((item_id, items_removed))
 
     confirm = input("Type YES to confirm: ")
     if confirm != "YES":
@@ -648,8 +627,7 @@ def restock_no_sticker(con):
 
 
 def event_handler(con, cmd):
-
-    if cmd.startswith("last"): # last sticker:
+    if cmd.startswith("last"):  # last sticker:
         show_max_sticker(con)
 
     if cmd.startswith("list"):
@@ -658,13 +636,16 @@ def event_handler(con, cmd):
     elif cmd.startswith("hide") or cmd.startswith("unhide"):
         hide(con, cmd)
 
+    elif cmd.startswith("putback"):
+        put_back(con, cmd)
+
     elif cmd.startswith("wadd"):
         warehouse_transaction(con, cmd)
 
     elif cmd.startswith("badd"):
         box_transaction(con, cmd)
 
-    elif cmd.startswith("brem"):  #boxremove
+    elif cmd.startswith("brem"):  # boxremove
         box_transaction(con, cmd)
 
     elif cmd.startswith("bi"):
@@ -673,10 +654,10 @@ def event_handler(con, cmd):
     elif cmd.startswith("wi"):
         show_inventory(con, "warehouse", cmd)
 
-    elif cmd.startswith("bt"): #, "bshow", "btrans"):
+    elif cmd.startswith("bt"):  # , "bshow", "btrans"):
         show_transactions(con, "box", cmd)
 
-    elif cmd.startswith("wt"): #, "wshow", "wtrans"):
+    elif cmd.startswith("wt"):  # , "wshow", "wtrans"):
         show_transactions(con, "warehouse", cmd)
 
     elif cmd.startswith("sql"):
@@ -709,16 +690,40 @@ def event_handler(con, cmd):
     elif cmd.startswith("diapers"):
         totals(con, summarized=False, diapers_only=True)
 
+
 def put_back(con, cmd):
     # parse out the sticker
+    item_id, quantity, sticker = check_cmd_line(cmd)
+    if not sticker:
+        error("Invalid sticker number")
+        return
+
     # check if it existed and if it isn't there now
+    sql = f"select quantity from box_inventory where sticker={sticker} and item_id={item_id}"
+    res = con.cursor().execute(sql)
+    row = res.fetchone()
+    if row is None:
+        error(f"Item {item_id} sticker {sticker} was not in the box.")
+        return
+
+    quantity = int(row[0])
+    if int(quantity) > 0:
+        error(f"Item {item_id} with sticker {sticker} is still there, you can't put it back.")
+        return
+
     # update box inventory and set quantity back to 1 for that sticker
+    con.cursor().execute(f"update box_inventory set quantity=1 where sticker={sticker}")
+
     # update warehouse inventory and add 1 back to quantity
-    # delete box transaction row and warehouse transaction row where you removed that item
-    pass
+    con.cursor().execute(f"update warehouse_inventory set quantity=quantity+1 where item_id={item_id}")
+
+    # delete box transaction row where you removed that item
+    con.cursor().execute(f"delete from box_transactions where quantity_changed=-1 and sticker={sticker}")
+
+    print("done")
+
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", help="database name, default is minipantry.db", default="minipantry.db")
 
